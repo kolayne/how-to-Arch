435b8ed876d06cb7e140f4fa8de11aa12c6f2068
Nikolai Nechaev <Nikolay_Nechaev@mail.ru>
2025-12-02 20:44:57 +0900

    Remove config files for i3, polybar, sway

--- _.config/i3/blank_screen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-SLEEPTIME=${1:-21}  # Screen will be off for at most this time
-type light >/dev/null || exit $?
-
-trap true HUP INT TERM QUIT
-trap "light -I" EXIT
-light -O
-light -S "$(light -P)"
-sleep "$SLEEPTIME" & wait
