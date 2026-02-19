65dece67e5b614442d2a36cb760f36d02f770563
Nikolai Nechaev <Nikolay_Nechaev@mail.ru>
2026-02-19 23:47:55 +0900

    fish: Ensure paging when showing AUR diffs
    Update `topgrade` and `yay` aliases to set the `LESS` env var
    to ensure `git diff`s of AUR packages to be updated are paged
    even when they fit on screen, so that no diffs are missed when
    updating multiple packages.

--- _.config/fish/functions/topgrade.fish
+++ _.config/fish/functions/topgrade.fish
@@ -1,4 +1,8 @@
-function topgrade --wraps topgrade --description 'Run topgrade with systemd-inhibit'
-  systemd-inhibit --what=shutdown:idle --why="System upgrade" topgrade $argv
-        
+function topgrade --wraps topgrade --description 'Run topgrade with tweaks'
+    # When yay shows diffs of AUR packages to be updated (via `git diff`),
+    # enable paging even when diffs fit on screen, so that nothing is missed.
+    # (git's default is -FRX, we don't want the -F)
+    set --local --export LESS -RX
+    # Inhibit shutdown and idleness
+    systemd-inhibit --what=shutdown:idle --why="System upgrade" topgrade $argv
 end
--- _.config/fish/functions/yay.fish
+++ _.config/fish/functions/yay.fish
@@ -1,4 +1,8 @@
-function yay --wraps yay --description 'Run yay with systemd-inhibit'
-  systemd-inhibit --what=shutdown:idle --why="Package installation/removal/upgrade" yay $argv
-        
+function yay --wraps yay --description 'Run yay with tweaks'
+    # When yay shows diffs of AUR packages to be updated (via `git diff`),
+    # enable paging even when diffs fit on screen, so that nothing is missed.
+    # (git's default is -FRX, we don't want the -F)
+    set --local --export LESS -RX
+    # Inhibit shutdown and idleness
+    systemd-inhibit --what=shutdown:idle --why="Package installation/removal/upgrade" yay $argv
 end
