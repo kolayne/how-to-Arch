3bc3a778504cd01c0753f5d35a5b0283738b5158
Nikolai Nechaev <Nikolay_Nechaev@mail.ru>
2026-08-28 19:54:42 +0400

    hyprland: Assign workspaces 7-9 to eDP-1

--- _.config/hypr/land/workspaces.lua
+++ _.config/hypr/land/workspaces.lua
@@ -1,10 +1,14 @@
+-- Workspace names and default monitor assignments
+
+require 'land.multimon'
+
 hl.workspace_rule({ workspace = "1", default_name = "一" })
 hl.workspace_rule({ workspace = "2", default_name = "二" })
 hl.workspace_rule({ workspace = "3", default_name = "三" })
 hl.workspace_rule({ workspace = "4", default_name = "四" })
 hl.workspace_rule({ workspace = "5", default_name = "五" })
 hl.workspace_rule({ workspace = "6", default_name = "六" })
-hl.workspace_rule({ workspace = "7", default_name = "七" })
-hl.workspace_rule({ workspace = "8", default_name = "八" })
-hl.workspace_rule({ workspace = "9", default_name = "九" })
+hl.workspace_rule({ workspace = "7", default_name = "七", monitor = BuiltinOutput })
+hl.workspace_rule({ workspace = "8", default_name = "八", monitor = BuiltinOutput })
+hl.workspace_rule({ workspace = "9", default_name = "九", monitor = BuiltinOutput })
 hl.workspace_rule({ workspace = "10", default_name = "十" })
